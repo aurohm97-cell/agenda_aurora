4ffd0e91b1318b3041c19aca1c6798210917347d
aurohm97-cell <aurohm97@gmail.com>
2026-06-18 12:45:36 +0200

    feat: add environment configuration files for development and production

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCBtm-8SPNxsD38rDljlKQj8Y5yqS7VtGU",
+    authDomain: "agenda-aurora-caf9b.firebaseapp.com",
+    projectId: "agenda-aurora-caf9b",
+    storageBucket: "agenda-aurora-caf9b.firebasestorage.app",
+    messagingSenderId: "393813414848",
+    appId: "1:393813414848:web:05b9b82c8f084b56b4ee25"
+  }
+};
